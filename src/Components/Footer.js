9df1d604ba6590df006d6165c49e7a232504ed12
Nacer MEDIOUNI <nacer.mediouni@gmail.com>
2022-05-12 23:37:32 +0100

    update the ui

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
 
 const Footer = () => {
+  var today = new Date();
+  var thisYear = today.getFullYear();
+
   return (
     <footer>
-      <p>Copyright &copy; 2021</p>
+      <p>Copyright &copy; {thisYear}</p>
       <Link to="/about">About</Link>
     </footer>
   );
